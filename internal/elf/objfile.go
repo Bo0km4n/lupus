29847517d438d26b44f2ef3b3489787c72512e10
Bo0km4n <pokopenkatuya@gmail.com>
2018-12-13 16:28:37 +0900

    tweak

--- internal/elf/objfile.go
+++ internal/elf/objfile.go
@@ -5,8 +5,6 @@ import (
 	"io"
 	"os"
 	"strings"
-
-	"github.com/k0kubun/pp"
 )
 
 type elfFile struct {
@@ -42,7 +40,6 @@ func GetFuncBytes(elfFile *elfFile, funcName string) []byte {
 	textSection := elfFile.elf.Section(".text")
 	fAddr := fSymbol.Value - textSection.Addr
 	textb := getSectionBytes(textSection)
-	pp.Println(len(textb), fAddr, fAddr+fSymbol.Size)
 	return textb[fAddr : fAddr+fSymbol.Size]
 }
 
